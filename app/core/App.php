e5b41e6f3c9b9d548562b43827aca4760560567c
Andrei Paunescu <andrei.paunescu@info.uaic.ro>
2019-06-13 03:47:07 +0300

    Added cookie

--- app/core/App.php
+++ app/core/App.php
@@ -1,5 +1,13 @@
 <?php
+$sessionTime = 1 * 24 * 60 * 60;
+$sessionName = "archivr";
+session_set_cookie_params($sessionTime);
+session_name($sessionName);
 session_start();
+
+if (isset($_COOKIE[$sessionName])) {
+    setcookie($sessionName, $_COOKIE[$sessionName], time() + $sessionTime, "/");
+}
 class App{
 
     protected $controller = 'Auth';
